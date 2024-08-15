e3f1c23c79bbda67bc4b252e764c2cc208b17fe6
Hanna-alex <ipantevaanna@gmail.com>
2024-08-15 22:37:33 +0300

    set up a project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+  text-align: center;
+`
+
+function App() {
+	return (
+		<Div>
+			<i className="fa fa-university" aria-hidden="true"></i>
+			<span>456</span>
+		</Div>
+	);
+}
+
+export default App;
+
+// cd ./project-00474
