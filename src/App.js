0a574e7c2ffc8f0684f1d7a526949ef80900c1d7
Hanna-alex <ipantevaanna@gmail.com>
2024-09-02 23:34:16 +0300

    the main component is implemented with routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { Routes, Route } from 'react-router-dom'
 import styled from 'styled-components'
 
-const Div = styled.div`
+const Content = styled.div`
+  padding:  120px 0
+`
+
+const H2 = styled.h2`
   text-align: center;
 `
 
-function App() {
+const Header = () => <div>Шапка</div>
+const Footer = () => <div>Футер</div>
+
+export const App = () => {
 	return (
-		<Div>
-			<i className="fa fa-university" aria-hidden="true"></i>
-			<span>456</span>
-		</Div>
+		<>
+			<Header />
+			<Content>
+				<H2>Контент страницы</H2>
+				<Routes>
+					<Route path='/' element={<div>Главная страница</div>} />
+					<Route path='/login' element={<div>Авторизация</div>} />
+					<Route path='/register' element={<div>Рагистрация</div>} />
+					<Route path='/hispory' element={<div>История операций</div>} />
+					<Route path='/settings' element={<div>Настройки</div>} />
+					<Route path='*' element={<div>Ошибка</div>} />
+				</Routes>
+			</Content>
+			<Footer />
+		</>
 	);
 }
 
-export default App;
 
 // cd ./project-00474
 // json-server --watch src/db.json --port 3007
